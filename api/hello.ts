1e729cba28ffca93a916b1560e0e7e0376ff0372
Tony Brierly <anthonylbrierly@gmail.com>
2025-08-20 16:35:34 -0700

    Update hello.ts

--- api/hello.ts
+++ api/hello.ts
@@ -2,6 +2,7 @@ import { VercelRequest, VercelResponse } from "@vercel/node";
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
 
+  console.log("Trying to say hello")
   try {
   
 
