0a6a411153b5a6d5e9a19139512d7a52b3d5a75b
Tony Brierly <anthonylbrierly@gmail.com>
2025-08-20 21:36:39 -0700

    module.exports syntax

--- api/hello.ts
+++ api/hello.ts
@@ -1,6 +1,6 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node'
 
-export default function handler(req: VercelRequest, res: VercelResponse) {
+module.exports = function handler(req: VercelRequest, res: VercelResponse) {
   const { name = 'World' } = req.query
   return res.json({
     message: `Hello ${name}!`,
