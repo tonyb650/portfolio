ffeaca121a11571b24b5cc39dff35012e5a4d090
Tony Brierly <anthonylbrierly@gmail.com>
2025-08-21 12:39:51 -0700

    Another experiment

--- dist-api/hello.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = handler;
-function handler(req, res) {
-    const { name = 'World' } = req.query;
-    return res.json({
-        message: `Hello ${name}!`,
-    });
-}
