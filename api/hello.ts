00800b6f882d6194b51891fceb7faa6ef7a8d10a
Tony Brierly <anthonylbrierly@gmail.com>
2025-08-20 21:41:03 -0700

    Try updating tsconfig.app.json

--- api/hello.ts
+++ api/hello.ts
@@ -1,6 +1,6 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node'
 
-module.exports = function handler(req: VercelRequest, res: VercelResponse) {
+export default function handler(req: VercelRequest, res: VercelResponse) {
   const { name = 'World' } = req.query
   return res.json({
     message: `Hello ${name}!`,
